45eccd1748941ecd73da8ae7b8afa18d2b6b3b12
Alexandru Ivan (ivana2) <ivana2@coventry.ac.uk>
2022-03-01 15:11:18 +0000

    Player  Stealth Controller

---
+++ 304CR-CW2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float runSpeed = 5;
+    public float walkSpeed = 1.8f;
+    public float rotationSpeed = 180;
+
+    public static bool IsStealth = false;
+
+    //Local 
+    Animator animator;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = transform.GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        CheckStealth();
+        Move();
+        Rotation();
+    }
+
+    void Move()
+    {
+        float verticalMove = Input.GetAxis("Vertical");
+
+        if (IsStealth)
+            transform.Translate(Vector3.forward * verticalMove * walkSpeed * Time.deltaTime, Space.Self);
+        else
+            transform.Translate(Vector3.forward * verticalMove * runSpeed * Time.deltaTime, Space.Self);
+
+        animator.SetFloat("MoveSpeed",verticalMove);
+    }
+
+    void Rotation()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalMove * rotationSpeed * Time.deltaTime);
+    }
+
+    void CheckStealth()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            IsStealth =!IsStealth;
+            animator.SetBool("IsStealth", IsStealth);
+
+        }
+    }
+}
